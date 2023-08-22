bc82448ebc73f2bb3338cc1ef291b682c3b51fac
mahamsshoaib <mahamsshoaib@gmail.com>
2023-08-22 02:27:35 +0100

    Created javaScript File

---
+++ clock.js
@@ -0,0 +1,17 @@
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+  
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+  }
+  
+  // Update the clock every second
+  setInterval(updateClock, 1000);
+  
+  // Call updateClock immediately to set the initial time
+  updateClock();
+  
